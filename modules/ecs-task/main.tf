042b9ddff041dc1d229d3af75ec9d1ad9bb849a0
cchowthi <christal.chowthi@gilead.com>
2026-06-12 11:44:22 -0400

    add secret retrieval to ecs service role

--- modules/ecs-task/main.tf
+++ modules/ecs-task/main.tf
@@ -28,6 +28,13 @@ locals {
           "awslogs-stream-prefix" : "${var.environment}-${var.app_name}"
         }
       }
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -f http://localhost:${var.app_port}/health || exit 1"]
+        interval    = 30
+        timeout     = 5
+        retries     = 3
+        startPeriod = 300
+      }
     },
     # TwistlockDefender sidecar container
     {
